6c9350259126c0fbf8fcbc1508d6cf8c70e2b801
Tyler Boyd <tylerb@theboydprojects.com>
2020-05-02 10:05:40 -0500

    changed routes to build directory

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const path = require('path');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use(favicon(__dirname + 'client/build/favicon.ico'));
+app.use(favicon(__dirname + '/client/build/favicon.ico'));
 
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
