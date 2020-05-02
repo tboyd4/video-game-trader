d044c5930e23c75b3f93725f459b925c5aeb0a2a
Tyler Boyd <tylerb@theboydprojects.com>
2020-05-02 10:11:42 -0500

    added comments to server.js

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
+// import statements for server.js
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 
+// variable declaration
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+// server.js middleware and use methods
 app.use(favicon(__dirname + '/client/build/favicon.ico'));
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, '/client/build')));
 
+// route that servers our production build out
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 
+// starts our server listening
 app.listen(PORT, () => {
     console.log('Server listening on PORT ' + PORT)
 });
