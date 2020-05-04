a8d380f8195d8dab1752b2f9a72b0c748b5b9d8d
Tyler Boyd <tylerb@theboydprojects.com>
2020-05-04 14:33:44 -0500

    fixed router functionality

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,53 +1,28 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Home from "../components/home/home";
 import BuySell from "../components/buysellPage/buySellPage";
 import User from "../components/userPage/userPage";
 import Login from "../components/authenticationPages/login";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello. I will someday become a video game trader</h1>
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Login</Link>
-              </li>
-              <li>
-                <Link to="/home">Home</Link>
-              </li>
-              <li>
-                <Link to="/user">User</Link>
-              </li>
-              <li>
-                <Link to="/buysell">Buy/Sell</Link>
-              </li>
-            </ul>
-          </nav>
-
-          <Switch>
-            <Route path="/">
-              <Login />
-            </Route>
-            <Route path="/home">
-              <Home />
-            </Route>
-            <Route path="/buysell">
-              <BuySell />
-            </Route>
-            <Route path="/user">
-              <User />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+        <h1>Hello. I will someday become a video game trader</h1>
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route exact path="/home" component={Home} />
+              <Route exact path="/buysell" component={BuySell} />
+              <Route exact path="/user" component={User} />
+            </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
