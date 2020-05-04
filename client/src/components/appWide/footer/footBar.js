54e33e9e7ec70f2170312092a5f9478a4cea5f6e
Tyler Boyd <tylerb@theboydprojects.com>
2020-05-04 15:34:30 -0500

    finished footbar style

--- client/src/components/appWide/footer/footBar.js
+++ client/src/components/appWide/footer/footBar.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function FootBar() {
     return (
-        <footer class="page-footer">
+        <footer class="page-footer green accent-3">
           <div class="container">
           </div>
           <div class="footer-copyright">
-            <div class="container">
+            <div class="container black-text">
             © 2020 Tyler Boyd, Jacob Borgen, Kyle Clark, Sheri Rhoades
             </div>
           </div>
