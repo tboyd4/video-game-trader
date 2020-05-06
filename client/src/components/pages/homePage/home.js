5b8af9a37ce3b0826493539b69e0e59ca7dfe2ef
clarkdkyle <clarkdkyle@gmail.com>
2020-05-05 21:00:28 -0500

    linked api in home

--- client/src/components/pages/homePage/home.js
+++ client/src/components/pages/homePage/home.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
+import API from "../../utils/API"
 import GameDisplay from '../../gameDisplay/gameDisplay';
+import SearchBar from "./SearchBar"
+
+
+function loadGames() {
+    API.multisearch(query)
+      .then(res => setGames(res.data)
+      )
+      .catch(err => console.log(err));
+    };
 
 function Home () {
     return (
         <main>
             <h1>I am Home Page</h1>
             <GameDisplay />
+            <SearchBar />
         </main>
     )
 }
