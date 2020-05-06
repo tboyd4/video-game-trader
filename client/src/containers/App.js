7e8fcb41a64ff98ac0a7facd069fc701857661b0
son loku <jacobrborgen@gmail.com>
2020-05-05 20:40:33 -0500

    added navbar component

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,28 +1,29 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 import Home from "../components/home/home";
 import BuySell from "../components/buysellPage/buySellPage";
 import User from "../components/userPage/userPage";
 import Login from "../components/authenticationPages/login";
 import FootBar from "../components/appWide/footer/footBar";
+import HeadBar from "../components/appWide/header/headBar";
 
 class App extends React.Component {
   render() {
     return (
+
       <div className="container">
-        <header>
-          <h1>Hello. I will someday become a video game trader</h1>
-        </header>
         
           <BrowserRouter>
+            <HeadBar>
             <Switch>
               <Route exact path="/" component={Login} />
               <Route exact path="/home" component={Home} />
               <Route exact path="/buysell" component={BuySell} />
               <Route exact path="/user" component={User} />
             </Switch>
+            </HeadBar>
           </BrowserRouter>
 
           <div className="filler"></div>
