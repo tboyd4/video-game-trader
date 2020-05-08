80f0571f01918b864417f9fb50654be515602767
son loku <jacobrborgen@gmail.com>
2020-05-07 20:16:18 -0500

    updated games model with more columns

--- backend/models/games.js
+++ backend/models/games.js
@@ -1,42 +1,47 @@
 module.exports = function (sequelize, DataTypes) {
   var Game = sequelize.define("Game", {
+
     title: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        len: [1],
-      },
     },
+
     console: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        len: [1],
-      },
     },
+
     price: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      validate: {
-        len: [1],
-      },
     },
+
+    releaseDate: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+
   });
 
-  Game.associate = (models) => {
-    Game.belongsTo(models.User, {
-      foreignKey: {
-        allowNull: false,
-      },
-    });
-   
-    // Game.associate = (models) => {
-    //     Game.belongsTo(models.User, {
-    //         foreignKey: {
-    //             allowNull: false
-    //         }
-    //     });
-    // };
+  // Game.associate = (models) => {
+  //   Game.belongsTo(models.User, {
+  //     foreignKey: {
+  //       allowNull: false,
+  //     },
+  //   });
+
+  // Game.associate = (models) => {
+  //     Game.belongsTo(models.User, {
+  //         foreignKey: {
+  //             allowNull: false
+  //         }
+  //     });
+  // };
 
   return Game;
 };
