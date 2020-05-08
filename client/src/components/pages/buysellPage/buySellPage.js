2708ea07672ca5a194be30f0b467a695b340134b
clarkdkyle <clarkdkyle@gmail.com>
2020-05-08 09:25:15 -0500

    tweaked sell&buysell page

--- client/src/components/pages/buysellPage/buySellPage.js
+++ client/src/components/pages/buysellPage/buySellPage.js
@@ -1,20 +1,50 @@
 import React from 'react'
-// import API from '../../../utils/API'
-
-// function loadGames() {
-//     API.idsearch(query)
-//         .then(res => setGames(res.data)
-//         )
-//         .catch(err => console.log(err));
-// };
+import API from '../../../utils/API'
+import SellDisplay from '../../gameDisplay/sellDisplay'
+import SearchBar from "./SearchBar/SearchBar"
+import SearchContext from '../../../utils/SearchContext'
+import SearchResults from './SearchResults'
 
 
 function BuySell() {
-    return (
-        <main>
-            <h1>I am BuySell Page</h1>
-        </main>
+    
+    const [search, setSearch] = useState({
+        result: ''
+    });
 
+    componentDidMount() {
+        this.searchGames("The Matrix");
+    }
+
+    searchGames = query => {
+        API.multisearch(query)
+            .then(res => this.setState({ result: res.data }))
+            .catch(err => console.log(err));
+    };
+
+    const handleInputChange = event => {
+        event.preventDefault();
+        setSearch({ result: event.target.value });
+        console.log(search.result)
+    };  
+
+    const handleFormSubmit = event => {
+        event.preventDefault();
+    };
+
+    return (
+        <SearchContext.Provider value={search}>
+            <main>
+                <h1>What games would you like to trade?</h1>
+                <SearchBar
+                    handleFormSubmit={handleFormSubmit}
+                    handleInputChange={handleInputChange}
+                    results={search}
+                />
+                <SearchResults />
+                <SellDisplay />
+            </main>
+        </SearchContext.Provider>
     )
 }
 
