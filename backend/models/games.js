9c5838c5a0e228b0071014d8a172b671feaf4bab
son loku <jacobrborgen@gmail.com>
2020-05-07 19:45:15 -0500

    forgot to fully update like a dummy

--- backend/models/games.js
+++ backend/models/games.js
@@ -29,7 +29,6 @@ module.exports = function (sequelize, DataTypes) {
         allowNull: false,
       },
     });
-<<<<<<< HEAD
    
     // Game.associate = (models) => {
     //     Game.belongsTo(models.User, {
@@ -38,9 +37,6 @@ module.exports = function (sequelize, DataTypes) {
     //         }
     //     });
     // };
-=======
-  };
->>>>>>> 7bbd005afa92d5d984995566d05da6a9dff185ac
 
   return Game;
 };
