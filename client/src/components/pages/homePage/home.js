82a09840371dac579797710040162d42d7e45f27
Tyler Boyd <tylerb@theboydprojects.com>
2020-05-14 18:48:02 -0500

    removed second home function and changed remove cart function error

--- client/src/components/pages/homePage/home.js
+++ client/src/components/pages/homePage/home.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import GameDisplay from '../../gameDisplay/gameDisplay';
 import SearchBar from "./SearchBar/SearchBar"
 import SearchResults from './SearchResults'
-import gamesAPI from '../../../utils/gamesAPI'
+import gamesAPI from '../../../utils/GamesAPI'
 
 
 function Home(props) {
@@ -51,37 +51,4 @@ function Home(props) {
     )
 }
 
-
-    function Home(props) {
-
-        const [search, setSearch] = useState({
-            result: ''
-        });
-
-        const handleInputChange = event => {
-            event.preventDefault();
-            setSearch({result: event.target.value});
-            console.log(search.result)
-        };
-        
-        const handleFormSubmit = event => {
-            event.preventDefault();
-        };
-
-        return (
-            <SearchContext.Provider value={search}>
-                <main>
-                    <SearchBar
-                        handleFormSubmit={handleFormSubmit}
-                        handleInputChange={handleInputChange}
-                        results={search}
-                    />
-
-                    <SearchResults/>
-                    <GameDisplay addCart={props.addCart} />
-                </main>
-            </SearchContext.Provider>
-        )
-    }
-
-    export default Home;
+export default Home;
