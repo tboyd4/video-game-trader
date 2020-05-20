0cb4e9540fe9c4174eadf56f7e254d4d1024a242
clarkdkyle <clarkdkyle@gmail.com>
2020-05-19 19:21:09 -0500

    getting api uptodate

--- client/src/utils/GamesAPI.js
+++ client/src/utils/GamesAPI.js
@@ -3,8 +3,8 @@ import axios from "axios";
 export default {
   // Gets all games
   getGames: function(search) {
-      console.log(`trying to get ${search}`)
-    return axios.get(`/api/games${search && search.length > 3 ? "?search=" + search : ""}`); 
+      console.log("trying to get!")
+    return axios.get(`/api/games${search && search.length > 3 ? "?search=" +search : ""}`);
   },
   // Gets the games with the given id
   getGame: function(id) {
@@ -18,5 +18,8 @@ export default {
   // Saves a game to the database
   saveGame: function(gameData) {
     return axios.post("/api/games", gameData).then(result => result.data)
+  },
+  savedGames: function () {
+    return axios.get("/api/game").then(result => result.data);
   }
 };
