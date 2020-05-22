6368b75aad13579bf02825ea21c1fe362e28c5c6
Sheri Rhoades <sheri_rhoades@yahoo.com>
2020-05-21 19:55:28 -0500

    add centaurs to nav bar

--- client/src/components/appWide/header/headBar.js
+++ client/src/components/appWide/header/headBar.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
-import './headBar.css';
+import "./headBar.css";
 // import Logo from '../../../images/proj3log.png';
 
 function HeadBar() {
   return (
     <nav>
       <div className="nav-wrapper green accent-3">
-        <a href="/home" className="brand-logo black-text" id="header-title">Centaur Game Trading</a>
+        <a href="/home" className="brand-logo black-text" id="header-title">
+          Centaur Game Trading
+        </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><Link className="black-text" to="/">Login</Link></li>
-          <li><Link className="black-text" to="/home">Home</Link></li>
-          <li><Link className="black-text" to="/buysell">Sell</Link></li>
-          <li><Link className="black-text" to="/cart">Cart</Link></li>
+          <li>
+            <Link className="black-text" to="/">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link className="black-text" to="/home">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="black-text" to="/buysell">
+              Sell
+            </Link>
+          </li>
+          <li>
+            <Link className="black-text" to="/cart">
+              Cart
+            </Link>
+          </li>
+          <li className="black-text" style={{ marginRight: "1rem" }}>
+            | Centaur Credits
+          </li>
         </ul>
       </div>
     </nav>
-
-  )
+  );
 }
 
 export default HeadBar;
--- client/src/utils/GamesAPI.js
+++ client/src/utils/GamesAPI.js
@@ -2,21 +2,31 @@ import axios from "axios";
 
 export default {
   // Gets all games
-  getGames: function(search) {
-      console.log(`trying to get ${search}`)
-    return axios.get(`/api/games${search && search.length > 3 ? "?search=" + search : ""}`); 
+  getGames: function (search) {
+    console.log(`trying to get ${search}`);
+    return axios.get(
+      `/api/games${search && search.length > 3 ? "?search=" + search : ""}`
+    );
   },
   // Gets the games with the given id
-  getGame: function(id) {
-    console.log("trying to get " + id)
+  getGame: function (id) {
+    console.log("trying to get " + id);
     return axios.get("/api/games/" + id);
   },
   // Deletes the games with the given id
-  deleteGame: function(id) {
+  deleteGame: function (id) {
     return axios.delete("/api/games/" + id);
   },
   // Saves a game to the database
-  saveGame: function(data) {
+  saveGame: function (data) {
     return axios.post("/api/games", data);
-  }
+  },
+  savedGames: function () {
+    return axios.get("/api/game").then((result) => result.data);
+  },
+
+  // money functions
+  removeMoney: function (payload) {
+    return axios.post("/api/removemoney", payload).then((result) => result);
+  },
 };
